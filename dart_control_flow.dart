7ae85ba699a3fb40b319882172990771399a1456
Ashraf <kwekaashraf@gmail.com>
2024-03-27 21:52:09 +0300

    comment added to the file

--- dart_control_flow.dart
+++ dart_control_flow.dart
@@ -1,5 +1,12 @@
+/*Create a Dart program that prompts the user for a number and 
+then prints a message to the console based on the following criteria:
+
+If the number is greater than 10, print "Your number is greater than 10"
+If the number is less than 10, print "Your number is less than 10"
+If the number is equal to 10, print "Your number is equal to 10"*/
+
 void main() {
-  int x = 10;
+  int x = 18;
   if (x > 10) {
     print("The number is greater than 10");
   } else if (x < 10) {
