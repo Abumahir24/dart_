55dcb12bd0054d5c2238c5aa2c74bc9271c64291
Ashraf <kwekaashraf@gmail.com>
2024-03-27 20:20:38 +0300

    Dart Control Flow Assignment

---
+++ dart_control_flow.dart
@@ -0,0 +1,10 @@
+void main() {
+  int x = 10;
+  if (x > 10) {
+    print("The number is greater than 10");
+  } else if (x < 10) {
+    print("The number is less than 10");
+  } else {
+    print("The number equals to 10");
+  }
+}
