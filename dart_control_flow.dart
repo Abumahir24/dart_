f225727bbed21df7584ca00a0f94fbacade54d56
Ashraf <kwekaashraf@gmail.com>
2024-04-05 12:54:49 +0300

    option to prompt user a number added

--- dart_control_flow.dart
+++ dart_control_flow.dart
@@ -4,12 +4,15 @@ then prints a message to the console based on the following criteria:
 If the number is greater than 10, print "Your number is greater than 10"
 If the number is less than 10, print "Your number is less than 10"
 If the number is equal to 10, print "Your number is equal to 10"*/
+import 'dart:io'; //allows input and output functionality
 
 void main() {
-  int x = 18;
-  if (x > 10) {
+  print("Enter the number:"); //prompts the user to enter the number
+  int num = int.parse(stdin.readLineSync()!); // Reads the user's input .
+
+  if (num > 10) {
     print("The number is greater than 10");
-  } else if (x < 10) {
+  } else if (num < 10) {
     print("The number is less than 10");
   } else {
     print("The number equals to 10");
